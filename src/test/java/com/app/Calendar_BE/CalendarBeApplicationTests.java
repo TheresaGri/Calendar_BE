b9ddaf1b2847900e08607b347b371e02c5bfa440
Theresa Grimus <T.Grimus@gmx.net>
2024-02-19 12:17:11 +0100

    initial commit

---
+++ src/test/java/com/app/Calendar_BE/CalendarBeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.app.Calendar_BE;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CalendarBeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
